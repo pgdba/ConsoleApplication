0ae0762cdbaaa7f98b3cf4b6b62bfd070bd0f473
Jakub Rapacz <j.rapacz@tvn.pl>
2015-04-30 14:09:20 +0200

    changed name handling in CommonTask

--- Hnk/ConsoleApplicationBundle/Task/CommonTask.php
+++ Hnk/ConsoleApplicationBundle/Task/CommonTask.php
@@ -5,9 +5,40 @@ namespace Hnk\ConsoleApplicationBundle\Task;
 abstract class CommonTask extends TaskAbstract implements RunnableTaskInterface
 {
     /**
+     * Task base name - e.g. 'vagrant up' or 'cache clear'
+     *
+     * Having BASE_NAME -> 'cache clear' and $name -> 'in my project', getName will return 'cache clear in my project'
+     *
+     */
+    const BASE_NAME = '';
+
+    /**
+     * @param string       $name
+     * @param array        $options
+     * @param string       $description
+     * @param TaskAbstract $parent
+     */
+    public function __construct($name = '', $options = array(), $description = '', TaskAbstract $parent = null)
+    {
+        parent::__construct($name, $options, $description, $parent);
+    }
+
+    /**
      * @return null
      */
     abstract public function handler();
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        if ('' !== static::BASE_NAME) {
+            return static::BASE_NAME . ' '. $this->name;
+        }
+
+        return $this->name;
+    }
 
     /**
      * @return null
