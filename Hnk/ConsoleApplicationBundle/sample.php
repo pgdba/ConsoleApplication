2c8d131d1e6e53c0c4c76ec5bb5785df1d208e96
pgdba <jaymzxii@gmail.com>
2015-05-27 15:17:01 +0200

    refactorization

--- Hnk/ConsoleApplicationBundle/sample.php
+++ Hnk/ConsoleApplicationBundle/sample.php
@@ -3,6 +3,15 @@
 
 use Hnk\ConsoleApplicationBundle\App;
 
+
+define('DEBUG_DIR', dirname(dirname(HNK_CONSOLE_APPLICATION_BASE_DIR)).'/Debug/Hnk/Debug');
+
+if (is_dir(DEBUG_DIR) && file_exists(DEBUG_DIR.'/bootstrap.php')) {
+    define('HNK_DEBUG_MODE', 'develop');
+
+    require_once DEBUG_DIR.'/bootstrap.php';
+}
+
 require_once __DIR__ . '/bootstrap.php';
 $GLOBALS['dev'] = 0;
 $appOptions = array(
